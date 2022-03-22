1e89e5bcd71c55c966e3cfb0b336d614715fa11e
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-03-22 17:02:57 -0400

    completed hmwk

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const { Router } = require('express')
+const controllers = require('../controllers')
+const router = Router()
+
+router.get('/plants/:id', controllers.getPlantById)
+
+router.get('/plants', controllers.getAllPlants)
+
+router.get('/', (req, res) => res.send('This is root!'))
+
+router.post('/plants', controllers.createPlant)
+
+router.put('/plants/:id', controllers.updatePlant)
+
+router.delete('/plants/:id', controllers.deletePlant)
+
+module.exports = router
